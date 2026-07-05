02927cabf6d07eb91481ac166a661fadf2c52ce2
Bhavesh Gadre <gadrebhawesh123@gmail.com>
2026-07-06 00:08:04 +0530

    ALB Security Group created

--- main.tf
+++ main.tf
@@ -112,3 +112,38 @@ resource "aws_route_table_association" "public_b_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+#ALB SECURITY GROUP#
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-security-group"
+  description = "Allow Http and HTTPS traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-security-group"
+  }
+}
+
