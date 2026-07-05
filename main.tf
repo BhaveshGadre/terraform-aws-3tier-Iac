177f921867e50d78951e1f31d906398527160af6
Bhavesh Gadre <gadrebhawesh123@gmail.com>
2026-07-06 00:41:23 +0530

    database security group created

--- main.tf
+++ main.tf
@@ -181,3 +181,32 @@ resource "aws_security_group" "webapp_sg" {
   }
 }
 
+#DATABASE SECURITY GROUP#   
+
+resource "aws_security_group" "db_sg" {
+  name        = "db-security-group"
+  description = "RDS Security Group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "MySQL from Web/App"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.webapp_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "db-security-group"
+  }
+}
+
+
+
