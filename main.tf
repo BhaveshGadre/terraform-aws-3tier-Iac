2555aa8a91f76083bed61580821495395e26a885
Bhavesh Gadre <gadrebhawesh123@gmail.com>
2026-07-05 23:20:57 +0530

    public and pvt subnet created

--- main.tf
+++ main.tf
@@ -8,3 +8,49 @@ resource "aws_vpc" "main" {
     }
 
 }
+
+#PUBLIC SUBNETS#
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-b"
+  }
+}
+
+#PRIVATE SUBNETS#
+
+resource "aws_subnet" "pvt_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "pvt-subnet-a"
+  }
+}
+
+resource "aws_subnet" "pvt_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "pvt-subnet-b"
+  }
+}
