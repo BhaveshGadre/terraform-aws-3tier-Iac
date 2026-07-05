76086658fb8941637a65f32ff4b7bd68664a5887
Bhavesh Gadre <gadrebhawesh123@gmail.com>
2026-07-06 00:26:40 +0530

    webapp security group

--- main.tf
+++ main.tf
@@ -147,3 +147,37 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
+#Web/App Security Group#
+
+resource "aws_security_group" "webapp_sg" {
+  name        = "webapp-security-group"
+  description = "Application server"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "HTTP from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+  ingress {
+    description     = "SSH"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks = ["47.11.7.69/32"]
+    }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "webapp-security-group"
+  }
+}
+
