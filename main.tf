499b4c1367b19e77fdf0cb487b16eb38e3a5cd81
Bhavesh Gadre <gadrebhawesh123@gmail.com>
2026-07-07 17:11:44 +0530

    Auto scaling group created

--- main.tf
+++ main.tf
@@ -295,5 +295,28 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+#AUTP SCALING GROUP#
+
+resource "aws_autoscaling_group" "webapp_asg" {
+  name                      = "webapp-asg"
+  max_size                  = 4
+  min_size                  = 2
+  desired_capacity          = 2
+  vpc_zone_identifier       = [aws_subnet.pvt_a.id, aws_subnet.pvt_b.id]
+  launch_template {
+    id      = aws_launch_template.webapp_lt.id
+    version = "$Latest"
+  }
+
+  target_group_arns         = [aws_lb_target_group.webapp_tg.arn]
+
+  health_check_type         = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "webapp-instance"
+    propagate_at_launch = true
+  }
+}
 
 
