08d0836705f9a5d08f59ef78b5ae22aed7ce0be0
Bhavesh Gadre <gadrebhawesh123@gmail.com>
2026-07-07 17:27:52 +0530

    elastic ip and nat gateway

--- main.tf
+++ main.tf
@@ -318,5 +318,52 @@ resource "aws_autoscaling_group" "webapp_asg" {
     propagate_at_launch = true
   }
 }
+#ELASTIC IP FOR NAT GATEWAY#
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+#NAT GATEWAY#
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id       = aws_subnet.public_a.id
+
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+#ROUTE TABLE FOR PRIVATE SUBNETS#
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+  tags = {
+    Name = "private-route-table"
+  } 
+}
+
+#ROUTE TABLE ASSOCIATION FOR PRIVATE SUBNETS#
+
+resource "aws_route_table_association" "private_a_assoc" {
+  subnet_id      = aws_subnet.pvt_a.id
+  route_table_id = aws_route_table.private_rt.id
+}  
+
+resource "aws_route_table_association" "private_b_assoc" {
+  subnet_id      = aws_subnet.pvt_b.id
+  route_table_id = aws_route_table.private_rt.id
+}
 
 
