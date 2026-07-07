cbede5282ad73b74e355ef92bca71f8bce547b44
Bhavesh Gadre <gadrebhawesh123@gmail.com>
2026-07-07 16:41:00 +0530

    ALB and ALB Listner Created

--- main.tf
+++ main.tf
@@ -266,5 +266,34 @@ resource "aws_lb_target_group" "webapp_tg" {
   }
 }
 
+#APPLICATION LOAD BALANCER#
+
+resource "aws_lb" "webapp_alb" {
+  name               = "webapp-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+
+  enable_deletion_protection = false  
+
+  tags = {
+    Name = "webapp-alb"
+  }
+}
+
+#ALB LISTENERS#
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.webapp_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.webapp_tg.arn
+    type             = "forward"
+  }
+}
+
 
 
